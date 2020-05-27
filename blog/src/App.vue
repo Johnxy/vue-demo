52b52adac7dafa241d1276d1bb91101bebc885d3
Johnxy Zheng <zhongxuzheng@gmail.com>
2020-05-28 05:20:36 +0800

    feat: 初始化工程

---
+++ blog/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Filters :date="curDate"/>
+  </div>
+</template>
+
+<script>
+import Filters from './components/Filters.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Filters,
+  },
+  data() {
+    return {
+      curDate: new Date()
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ blog/src/components/Filters.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <span>时间：{{ date | dateFormat }}</span>
+    <span v-pre> {{ preText }} </span>
+    <span v-html="preText"> </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Filters',
+  props: {
+    date: String
+  },
+  filters: {
+    dateFormat(value) {
+      let date = new Date(value);
+      return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    },
+  },
+  data() {
+    return {
+      preText: '<div>过滤器</div>'
+    };
+  }
+}
+</script>
